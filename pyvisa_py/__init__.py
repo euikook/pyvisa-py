de06c76a40668ced214978b3a337a35b2ce88917
MatthieuDartiailh <marul@laposte.net>
2020-07-10 22:07:22 -0400

    pyvisa_py: rename main package and update type annotations

--- pyvisa_py/__init__.py
+++ pyvisa_py/__init__.py
@@ -6,9 +6,11 @@
 :license: MIT, see LICENSE for more details.
 
 """
-try:
+import sys
+
+if sys.version_info >= (3, 8):
     from importlib.metadata import version, PackageNotFoundError
-except ImportError:
+else:
     from importlib_metadata import version, PackageNotFoundError  # type: ignore
 
 __version__ = "unknown"
